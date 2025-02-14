e65460564f7b22cd2b0eecad9b3d6da342af9f04
iBonChun <rmplorita@gmail.com>
2025-02-15 01:57:59 +0800

    feat: add js es6

--- arrayMethods.js
+++ arrayMethods.js
@@ -58,3 +58,6 @@ console.log(hasEven);
 const numbers5 = [2, 4, 6, 8];
 const allEven = numbers.every(num => num % 2 === 0);
 console.log(allEven);
+
+//find()
+
---
+++ ternaryExpression.js
@@ -0,0 +1,11 @@
+const num = 10;
+const result = num > 5 ? '大於5' : '小於或等於5';
+console.log(result);
+
+const number = 8;
+const result1 = number % 2 === 0 ? '偶數' : '奇數';
+console.log(result1);
+
+const isLoggIn = false;
+const message = isLoggIn ? '歡迎回來' : '請先登入';
+console.log(message);
